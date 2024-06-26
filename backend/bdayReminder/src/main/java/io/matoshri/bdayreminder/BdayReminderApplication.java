87c8f8d55ef3c1f92eb4b9fbadb92f6196bd909f
Prakash Rao <prakash8876@gmail.com>
2024-06-27 00:50:16 +0530

    backend: Setting default static property for colour console

--- backend/bdayReminder/src/main/java/io/matoshri/bdayreminder/BdayReminderApplication.java
+++ backend/bdayReminder/src/main/java/io/matoshri/bdayreminder/BdayReminderApplication.java
@@ -11,6 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BdayReminderApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.output.ansi.enabled","always");
+        //--spring.output.ansi.enabled=ALWAYS
         SpringApplication.run(BdayReminderApplication.class, args);
     }
 
